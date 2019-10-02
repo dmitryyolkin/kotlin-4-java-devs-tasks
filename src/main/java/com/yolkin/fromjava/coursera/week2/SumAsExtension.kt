3570e27db77a21a0b73675a80e1c5a6ad865465d
Dmitry <dmitry.yolkin@maxifier.com>
2019-10-02 10:14:17 +0400

    Example extension function

---
+++ src/main/java/com/yolkin/fromjava/coursera/week2/SumAsExtension.kt
@@ -0,0 +1,24 @@
+package com.yolkin.fromjava.coursera.week2
+
+fun sumAsFunction(list: List<Int>): Int {
+    var result = 0
+    for (i in list) {
+        result += i
+    }
+    return result
+}
+
+// sum as extension function
+// of standard sumAsFunction method
+fun List<Int>.sum(): Int {
+    var result = 0
+    for (i in this) {
+        result += i
+    }
+    return result
+}
+
+fun main(args: Array<String>) {
+    val sum = listOf(1, 2, 3).sum()
+    println(sum)    // 6
+}
