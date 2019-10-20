aeb53a110c9e9a3d70c990d383a450c302e43659
Dmitry <dmitry.yolkin@maxifier.com>
2019-10-20 15:51:01 +0400

    Coursera week4 task 2 (OOP + class inheritance)
    Square and Game boards

---
+++ src/main/java/com/yolkin/fromjava/coursera/week4/board/SquareBoardImpl.kt
@@ -0,0 +1,79 @@
+package board
+
+import board.Direction.*
+import kotlin.math.abs
+
+open class SquareBoardImpl(final override val width: Int) : SquareBoard {
+    private val board: List<List<Cell>>
+
+    init {
+        val rows = mutableListOf<List<Cell>>()
+        // Note that the numbering of cells starts with 1 (not 0)
+        for (i in 1..width) {
+            val columns = mutableListOf<Cell>()
+            for (j in 1..width) {
+                columns.add(Cell(i, j))
+            }
+            rows.add(columns)
+        }
+        // make board immutable
+        this.board = rows.toList()
+    }
+
+    override fun getCellOrNull(i: Int, j: Int): Cell? {
+        if (i < 1 || i > board.size) {
+            return null
+        }
+
+        val columns = board[i - 1]
+        return if (j < 1 || j > columns.size) null else columns[j - 1]
+    }
+
+    override fun getCell(i: Int, j: Int): Cell {
+        return getCellOrNull(i, j) ?: throw IllegalArgumentException("Cell is null")
+    }
+
+    override fun getAllCells(): Collection<Cell> {
+        return board
+                .flatten()
+                .toList()
+    }
+
+    override fun getRow(i: Int, jRange: IntProgression): List<Cell> {
+        if (board.size < i) {
+            // return empty list
+            return listOf()
+        }
+
+        val columns = board[i - 1]
+        val result = mutableListOf<Cell>()
+        for (j in jRange) {
+            if (columns.size >= j) {
+                result.add(columns[j - 1])
+            }
+        }
+        return result.toList()
+    }
+
+    override fun getColumn(iRange: IntProgression, j: Int): List<Cell> {
+        val result = mutableListOf<Cell>()
+        for (i in iRange) {
+            if (board.size >= i) {
+                val columns = board[i - 1]
+                if (columns.size >= j) {
+                    result.add(columns[j - 1])
+                }
+            }
+        }
+        return result.toList()
+    }
+
+    override fun Cell.getNeighbour(direction: Direction): Cell? {
+        return when (direction) {
+            UP -> getCellOrNull(i - 1, j)
+            DOWN -> getCellOrNull(i + 1, j)
+            LEFT -> getCellOrNull(i, j - 1)
+            RIGHT -> getCellOrNull(i, j + 1)
+        }
+    }
+}
